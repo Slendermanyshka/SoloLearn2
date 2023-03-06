a7714d23a304f7a535a2543ee88f0eb6138d0ff6
Slendermanyshka <42029005+Slendermanyshka@users.noreply.github.com>
2023-03-07 01:35:51 +0200

    half of 1st task iss done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click',onPrevClick);
+
+galleryEl.insertAdjacentHTML("beforeend", createImgCard(galleryItems));
+
+
+
+function createImgCard(galItems){
+ return galItems.map(({preview, original, description})=>{
+
+    return `
+    <div class="gallery-item">
+    <img src="${preview}" data-source="${original}" alt="${description}" height=100% width=100%>
+    </div>
+    `
+ }).join("");}
+
+
+ function onPrevClick(e){
+    const isGalItem = e.target.classList.contains('gallery-item');
+    if(!isGalItem){
+        return;
+    }
+    console.log(e.target)
+ }
+
